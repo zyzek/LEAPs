61fa2d6eaffc4d5e17425086fb191bcc56a39f5c
Guillaume Picard <wilhearts@gmail.com>
2021-07-12 11:14:55 +0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "snx-sips",
+  },
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
