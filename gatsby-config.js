ccb02b80fbcd1398e63d3d6bc3e05d4ce39e98d7
Guillaume Picard <wilhearts@gmail.com>
2021-07-16 16:23:38 +0700

    FIX gql loop on develop

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-graphql-codegen',
       options: {
-        fileName: './src/gql.ts',
+        fileName: './types/gql.ts',
       },
     },
     {
